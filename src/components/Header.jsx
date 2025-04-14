ddd46b1cb5084d7f9233315b7afee76e06e4ce38
Oscar707 <kow123@hotmail.ca>
2025-04-13 21:53:13 -0400

    fixed links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,15 @@ const Header = () => {
     <header className="w-full bg-gray-900 text-white p-5">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center">
         {/* Main title */}
-        <h1 className="text-2xl font-bold">
-          OSCAR CHEN
-        </h1>
+        <a href="#/" className="text-lg transition-colors">
+            <div className="flex items-center">
+              <img 
+                src="/logo.svg" 
+                alt="Logo" 
+                className="h-8 w-auto"
+              />
+            </div>
+          </a>
         
         {/* Navigation */}
         <nav>
@@ -21,9 +27,14 @@ const Header = () => {
             <a href="/photos" className="text-lg hover:text-gray-400 hover:translate-x-1 transition-all duration-200">
               Photos
             </a>
-            <a href="/resume" className="text-lg hover:text-gray-400 hover:translate-x-1 transition-all duration-200">
-              Resume
-            </a>
+            <a 
+                href="/Oscar_CV25.pdf" 
+                target="_blank" 
+                rel="noopener noreferrer" 
+                className="text-lg hover:text-gray-400 hover:translate-x-1 transition-all duration-200"
+              >
+                Resume
+              </a>
           </div>
         </nav>
       </div>
